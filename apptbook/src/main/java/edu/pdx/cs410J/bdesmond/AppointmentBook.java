6420909081efb1a62eb22c99d44f17e583df05bc
Bennett Desmond <bennett.a.desmond@gmail.com>
2021-06-26 18:45:11 -0700

    New AppointmentBook class started

---
+++ apptbook/src/main/java/edu/pdx/cs410J/bdesmond/AppointmentBook.java
@@ -0,0 +1,22 @@
+package edu.pdx.cs410J.bdesmond;
+
+import edu.pdx.cs410J.AbstractAppointmentBook;
+
+import java.util.Collection;
+
+public class AppointmentBook extends AbstractAppointmentBook {
+    @Override
+    public String getOwnerName() {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public Collection<T> getAppointments() {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+
+    @Override
+    public void addAppointment(T var1) {
+        throw new UnsupportedOperationException("This method is not implemented yet");
+    }
+}
