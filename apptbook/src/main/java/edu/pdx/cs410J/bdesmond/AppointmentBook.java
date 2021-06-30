a078277d74d14563a3b2f641ffe0686505075e0c
Bennett Desmond <bennett.a.desmond@gmail.com>
2021-06-29 19:07:49 -0700

    Work done on the Koans and some work done on the AppointmentBook Class. This class now has more fields.

--- apptbook/src/main/java/edu/pdx/cs410J/bdesmond/AppointmentBook.java
+++ apptbook/src/main/java/edu/pdx/cs410J/bdesmond/AppointmentBook.java
@@ -1,22 +1,38 @@
 package edu.pdx.cs410J.bdesmond;
 
+import edu.pdx.cs410J.AbstractAppointment;
 import edu.pdx.cs410J.AbstractAppointmentBook;
 
 import java.util.Collection;
 
 public class AppointmentBook extends AbstractAppointmentBook {
+    private String owner;
+    private Appointment[] appointments;
+
+    public AppointmentBook() {
+        super();
+        owner = "";
+        appointments = new Appointment[10]; //This number will most likely change
+    }
+
+    public AppointmentBook(String owner, Appointment[] appointments) {
+        super();
+        this.owner = owner;
+        //I believe I will need a function to copy over the data to the new array.
+        this.appointments = appointments;
+    }
     @Override
     public String getOwnerName() {
         throw new UnsupportedOperationException("This method is not implemented yet");
     }
 
     @Override
-    public Collection<T> getAppointments() {
+    public Collection<AbstractAppointment> getAppointments() {
         throw new UnsupportedOperationException("This method is not implemented yet");
     }
 
     @Override
-    public void addAppointment(T var1) {
-        throw new UnsupportedOperationException("This method is not implemented yet");
+    public void addAppointment(AbstractAppointment abstractAppointment) {
+
     }
 }
