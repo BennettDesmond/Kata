a5062c24ea48d6f19645cf7e045394ec1aaba346
Bennett Desmond <bennett.a.desmond@gmail.com>
2021-07-07 18:58:21 -0700

    Added divisibility by 3 rule to the program.

--- FooBarQuix/src/main/java/edu/pdx/cs410J/Mauckmond/Kata.java
+++ FooBarQuix/src/main/java/edu/pdx/cs410J/Mauckmond/Kata.java
@@ -13,4 +13,12 @@ public class Kata {
     System.err.println("Missing command line arguments");
     System.exit(1);
   }
+
+  public static String compute(String str) {
+    String returnStr = str;
+    if((Integer.parseInt(str) % 3) == 0) {
+      returnStr = "Foo";
+    }
+    return returnStr;
+  }
 }
--- FooBarQuix/src/test/java/edu/pdx/cs410J/Mauckmond/KataTest.java
+++ FooBarQuix/src/test/java/edu/pdx/cs410J/Mauckmond/KataTest.java
@@ -2,12 +2,22 @@ package edu.pdx.cs410J.Mauckmond;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
 public class KataTest
 {
 
   @Test
   void canInstantiateKataClass() {
     new Kata();
+  }
+
+  @Test
+  void divisibleByThree() {
+    assertThat(Kata.compute("3"), containsString("Foo"));
+    assertThat(Kata.compute("6"), containsString("Foo"));
+    assertThat(Kata.compute("9"), containsString("Foo"));
   }
 
 }
